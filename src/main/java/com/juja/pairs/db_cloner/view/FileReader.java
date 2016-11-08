6d716a0ae1b5aefe88e80883136f70a970d3d78a
Alex <sumy.ua@gmail.com>
2016-11-08 07:09:42 +0200

    bash test

--- src/main/java/com/juja/pairs/db_cloner/view/FileReader.java
+++ src/main/java/com/juja/pairs/db_cloner/view/FileReader.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 public class FileReader {
+    public static final String SEPARATOR = System.lineSeparator();
     private String filePath;
 
     public FileReader(String filePath) {
@@ -16,8 +17,7 @@ public class FileReader {
 
         try (BufferedReader in = new BufferedReader(new java.io.FileReader(filePath))){
             while ((readLine = in.readLine()) != null){
-                String separator = System.lineSeparator();
-                builder.append(readLine).append(separator);
+                builder.append(readLine).append(SEPARATOR);
             }
           } catch (IOException io){
             io.printStackTrace();
